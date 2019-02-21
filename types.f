9698ee1db6a2fecc60fea6ae59d36f0e47a3e0c1
Unknown <roger.levy@gmail.com>
2019-02-21 18:30:17 -0500

    add AABB@

--- types.f
+++ types.f
@@ -22,3 +22,5 @@ struct %rect
 : x2@   dup x@ swap w@ + ;        : x2!   >r r@ x@ - r> w! ;
 : y2@   dup y@ swap h@ + ;        : y2!   >r r@ y@ - r> h! ;
 : xy2@  dup 2@ rot wh@ 2+ ;       : xy2!  >r r@ xy@ 2- r> wh! ;
+
+: aabb@ xywh@ 2over 2+ ;
